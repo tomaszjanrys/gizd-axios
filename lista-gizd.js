ef3b0fe54c097ec09660aed058febd64633cde17
tomaszjanrys <tomaszjanrys@gmail.com>
2020-01-21 12:44:16 +0100

    ok

---
+++ lista-gizd.js
@@ -0,0 +1,9 @@
+const tomek = axios('require');
+const z = axios.created({
+    baseURL: 'https://api.github.com/',
+    responseType: 'json',
+    headers: {
+        'Authorization': 'token 21f1853c04aa27b80c236b65571f7c1a7dfa1ada',
+        'Accept': 'application/vnd.github.v3+json',
+    }
+})
