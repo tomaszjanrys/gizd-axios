f78e3076ce15615dcec43c73ba273323104ff5de
tomaszjanrys <tomaszjanrys@gmail.com>
2020-01-21 12:11:45 +0100

    zmina

--- gizd-create-axios.js
+++ gizd-create-axios.js
@@ -3,7 +3,7 @@ const tomek = axios.create({
     baseURL: 'https://api.github.com/',
     responseType: 'json',
     headers: {
-        'Authorization': 'token 66598019ec2a6372a5a2d58461aabdd17463bfbe',
+        'Authorization': 'token 21f1853c04aa27b80c236b65571f7c1a7dfa1ada',
         'Accept': 'application/vnd.github.v3+json',
     }
 });
@@ -11,7 +11,7 @@ const tomek = axios.create({
 const x = tomek.post('/gists', {
         "description": "Gizd 1",
         "public": true,
-        "files": { "j.txt": { "content": "jednocześnie utwożone dwa gizdy" } }
+        "files": { "gizd.txt": { "content": "jednocześnie utworzone dwa gizdy." } }
     })
     .then(function(response) {
         console.log(response.data);
@@ -23,7 +23,7 @@ const x = tomek.post('/gists', {
 const y = tomek.post('/gists', {
         "description": "Gizd 2",
         "public": true,
-        "files": { "j.txt": { "content": "jednocześnie utwożone dwa gizdy" } }
+        "files": { "gizd.txt": { "content": "jednocześnie utworzone dwa gizdy." } }
 
     })
     .then(function(response) {
