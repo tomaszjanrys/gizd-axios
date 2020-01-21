da00678c8d6515a8e8005ca866859668a6a295a9
tomaszjanrys <tomaszjanrys@gmail.com>
2020-01-21 16:10:24 +0100

    dodaje komentarz do gizda

--- add-comment.js
+++ add-comment.js
@@ -3,13 +3,13 @@ const z = tomek.create({
     baseURL: 'https://api.github.com/',
     responseType: 'json',
     headers: {
-        'Authorization': 'token c962ab7bf9ee61450c802ced2935bfafb1ca760b',
+        'Authorization': 'token c43019e45a406dd2f5842f70a14025989e26d20f',
         'Accept': 'application/vnd.github.v3+json',
     }
 })
 
 
-z.post('/gists/70341ab26fae042b490d85da3cbe5dda/comments', {
+z.post('/gists/88b3e3ed3dd4aba2affb1daca7c926b7/comments', {
         "body": "Znasz gita i jest git."
     })
     .then(function(response) {
