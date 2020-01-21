0e8cb8e847c3214b695e6a6e8442b4f06c86715f
tomaszjanrys <tomaszjanrys@gmail.com>
2020-01-21 15:24:40 +0100

    edytuje gista

--- edit-gist.js
+++ edit-gist.js
@@ -3,15 +3,18 @@ const z = tomek.create({
     baseURL: 'https://api.github.com/',
     responseType: 'json',
     headers: {
-        'Authorization': 'token c56cd8ed761623ae98d8e6cd6cfa48b0dd1c76e3',
+        'Authorization': 'token bc1d98d889b7f959b4c19afdcfad5f89a1a20934',
         'Accept': 'application/vnd.github.v3+json',
     }
 })
 
 
-z.patch('/gists/70341ab26fae042b490d85da3cbe5dda' {
-        "gizd.txt": null,
-        "new_file.txt": { "content": "Edytacja ok" }
+z.patch('/gists/70341ab26fae042b490d85da3cbe5dda', {
+        "files": {
+            "gizd.txt": {
+                "content": "edit ok."
+            }
+        }
     })
     .then(function(response) {
         console.log(response);
